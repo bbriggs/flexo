a247e934f7c1f436219b7f825c7a062e5e0c312c
MOREAU Olivier <m242@pm.me>
2021-02-28 20:46:29 +0100

    some bug fixes on faker

--- faker/main.go
+++ faker/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bxcodec/faker/v3"
+	"gorm.io/gorm"
 
 	"flexo/model"
 	"flexo/util"
@@ -12,9 +15,9 @@ func main() {
 	db := util.DBconnect(
 		util.LookupEnv("FLEXO_DB_USER", "flexo"),
 		util.LookupEnv("FLEXO_DB_PASS", "flexo"),
-		util.LookupEnv("FLEXO_DB_HOST", "localhost"),
+		util.LookupEnv("FLEXO_DB_HOST", "localhost:5432"),
 		util.LookupEnv("FLEXO_DB_NAME", "flexo"),
-		util.LookupEnv("FLEXO_DB_SSLMODE", false),
+		/*util.LookupEnv("FLEXO_DB_SSLMODE", false)*/ false,
 	)
 
 	categoryCount := 15
@@ -27,7 +30,8 @@ func main() {
 func fakeCategories(db *gorm.DB, count int) {
 	for i := 0; i < count; i++ {
 		category := model.Category{}
-		err := faker.FakeData(&category)
+		//err := faker.FakeData(&category)
+		faker.FakeData(&category)
 		res := db.Create(&category)
 		if res.Error != nil {
 			fmt.Println(res.Error)
@@ -38,7 +42,8 @@ func fakeCategories(db *gorm.DB, count int) {
 func fakeTeams(db *gorm.DB, count int) {
 	for i := 0; i < count; i++ {
 		team := model.Team{}
-		err := faker.FakeData(&team)
+		//err := faker.FakeData(&team)
+		faker.FakeData(&team)
 		res := db.Create(&team)
 		if res.Error != nil {
 			fmt.Println(res.Error)
