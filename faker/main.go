cbded3695907e9b5e55923879a1d1a906c75979d
Bren Briggs <code@fraq.io>
2021-02-28 12:21:54 -0500

    Add data faking for teams and categories

--- faker/main.go
+++ faker/main.go
@@ -2,7 +2,39 @@ package main
 
 import (
 	"github.com/bxcodec/faker/v3"
-	//	"flexo/model"
+
+	"flexo/model"
+	"flexo/util"
 )
 
-func main() {}
+func main() {
+	db := model.DBconnect("postgres", "flexo", "postgres", "flexo", false)
+
+	categoryCount := 15
+	teamCount := 100
+
+	fakeCategories(db, categoryCount)
+	fakeTeams(db, teamCount)
+}
+
+func fakeCategories(db *gorm.DB, count int) {
+	for i := 0; i < count; i++ {
+		category := model.Category{}
+		err := faker.FakeData(&category)
+		res := db.Create(&category)
+		if res.Error != nil {
+			fmt.Println(res.Error)
+		}
+	}
+}
+
+func fakeTeams(db *gorm.DB, count int) {
+	for i := 0; i < count; i++ {
+		team := model.Team{}
+		err := faker.FakeData(&team)
+		res := db.Create(&team)
+		if res.Error != nil {
+			fmt.Println(res.Error)
+		}
+	}
+}
