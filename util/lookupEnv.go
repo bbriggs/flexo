fd5f904d9c3a7c4c83d3d6ba4eb8cca5d4e30a2b
MOREAU Olivier <m242@pm.me>
2021-02-28 19:28:48 +0100

    fixing imports

--- util/lookupEnv.go
+++ util/lookupEnv.go
@@ -1,5 +1,9 @@
 package util
 
+import (
+	"os"
+)
+
 // looks up an environment variable and returns the var or a fallback
 func LookupEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
