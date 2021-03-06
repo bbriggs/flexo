2b3584c2d297342ec5dcc89a2b0f2dcd63082a52
Olivier Moreau <m242@pm.me>
2021-03-06 20:04:58 +0100

    Feature/team report (#26)
    * Score calculation
    
    * report/team/:ID works, but needs refactoring
    
    * shuffled bits of code around to make it more readable
    
    * updated readme
    
    * logging instead of failing

---
+++ flexo/reports.go
@@ -0,0 +1,53 @@
+package flexo
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+
+	"flexo/model"
+)
+
+func (s *Server) teamReport(c *gin.Context) {
+	id_str := c.Param("ID")
+
+	id, err := strconv.Atoi(id_str)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "ID isn't an int")
+		return
+	}
+
+	baseMultiplier := 5
+	score := 0
+
+	timeline, err := s.fetchTeamTimeline(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, "couldn't fetch timeline")
+		return
+	}
+
+	targs := []int{}
+	for _, e := range timeline {
+		sc, err := s.computeEventValue(e.Category, baseMultiplier)
+		if err != nil {
+			fmt.Printf("Couldn't compute event %d's value\n", e.ID)
+		}
+		score += sc
+
+		for _, t := range e.Targets {
+			targs = append(targs, int(t))
+		}
+	}
+
+	targets, err := s.getTargetList(targs)
+
+	report := model.TeamReport{
+		Score:    score,
+		Timeline: timeline,
+		Targets:  targets,
+	}
+
+	c.JSON(http.StatusOK, report)
+}
---
+++ model/reports.go
@@ -0,0 +1,7 @@
+package model
+
+type TeamReport struct {
+	Score    int
+	Timeline []Event
+	Targets  []Target
+}
