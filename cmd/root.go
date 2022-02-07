670de139404117f408e848ef4a78593248747ee3
Bren Briggs <code@fraq.io>
2022-02-06 20:12:03 -0500

    Update help text for rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -37,13 +37,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "flexo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Flexo - CCDC Red Team Scoring API",
 	Run: func(cmd *cobra.Command, args []string) {
 		if viper.GetString("dbPass") == "" && os.Getenv("DATABASE_URL") == "" {
 			fmt.Println("Password not provided and DATABASE_URL environment varialbe not set. Exiting!")
